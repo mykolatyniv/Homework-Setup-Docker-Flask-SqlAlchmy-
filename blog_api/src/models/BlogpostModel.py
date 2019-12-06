d35e2fbc6d2bb05ac91bce0b3594f4659ffcccd2
mykolatyniv <ml546@njit.edu>
2019-12-06 14:31:34 -0500

    Task: updated BlogpostModel file

--- blog_api/src/models/BlogpostModel.py
+++ blog_api/src/models/BlogpostModel.py
@@ -1,48 +1,48 @@
 # src/models/BlogpostModel.py
-from . import db
-import datetime
-
+#####################
+# existing code remains #
+########################
+from marshmallow import fields, Schema
 
 class BlogpostModel(db.Model):
-    """
+  """
   Blogpost Model
   """
 
-    __tablename__ = 'blogposts'
+  __tablename__ = 'blogposts'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    contents = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime)
-    modified_at = db.Column(db.DateTime)
+  #####################
+  # existing code remains #
+  ########################
+  contents = db.Column(db.Text, nullable=False)
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # add this new line
+  #####################
+  # existing code remains #
+  ########################
 
-    def __init__(self, data):
-        self.title = data.get('title')
-        self.contents = data.get('contents')
-        self.created_at = datetime.datetime.utcnow()
-        self.modified_at = datetime.datetime.utcnow()
+  def __init__(self, data):
+    #####################
+    # existing code remains #
+    ########################
+    self.owner_id = data.get('owner_id) # add this new line
+    self.created_at = datetime.datetime.utcnow()
+    self.modified_at = datetime.datetime.utcnow()
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+  #####################
+  # existing code remains #
+  ########################
 
-    def update(self, data):
-        for key, item in data.items():
-            setattr(self, key, item)
-        self.modified_at = datetime.datetime.utcnow()
-        db.session.commit()
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
 
-    def delete(self):
-        db.session.delete(self)
-        de.session.commit()
-
-    @staticmethod
-    def get_all_blogposts():
-        return BlogpostModel.query.all()
-
-    @staticmethod
-    def get_one_blogpost(id):
-        return BlogpostModel.query.get(id)
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+# add this new class
+class BlogpostSchema(Schema):
+  """
+  Blogpost Schema
+  """
+  id = fields.Int(dump_only=True)
+  title = fields.Str(required=True)
+  contents = fields.Str(required=True)
+  owner_id = fields.Int(required=True)
+  created_at = fields.DateTime(dump_only=True)
+  modified_at = fields.DateTime(dump_only=True)
