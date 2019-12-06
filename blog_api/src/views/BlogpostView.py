39b53d0b412682317569bdf76888711180b5464a
mykolatyniv <ml546@njit.edu>
2019-12-06 14:45:55 -0500

    Task: updated blogpostfile file with new information

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -1,4 +1,4 @@
-# /src/views/BlogpostView.py
+#/src/views/BlogpostView.py
 # app initiliazation
 #####################
 # existing code remain #
@@ -8,15 +8,14 @@
 @blogpost_api.route('/', methods=['POST'])
 @Auth.auth_required
 def create():
-    """
+  """
   Create Blogpost Function
   """
-    # app initiliazation
-    #####################
-    # existing code remain #
-    ######################
-    return custom_response(data, 201)
-
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
+  return custom_response(data, 201)
 
 # app initiliazation
 #####################
@@ -24,24 +23,18 @@ def create():
 ######################
 
 
-@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
 @Auth.auth_required
-def update(blogpost_id):
-    """
-  Update A Blogpost
+def delete(blogpost_id):
   """
-    req_data = request.get_json()
-    post = BlogpostModel.get_one_blogpost(blogpost_id)
-    if not post:
-        return custom_response({'error': 'post not found'}, 404)
-    data = blogpost_schema.dump(post).data
-    if data.get('owner_id') != g.user.get('id'):
-        return custom_response({'error': 'permission denied'}, 400)
+  Delete A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  if data.get('owner_id') != g.user.get('id'):
+    return custom_response({'error': 'permission denied'}, 400)
 
-    data, error = blogpost_schema.load(req_data, partial=True)
-    if error:
-        return custom_response(error, 400)
-    post.update(data)
-
-    data = blogpost_schema.dump(post).data
-    return custom_response(data, 200)
+  post.delete()
+  return custom_response({'message': 'deleted'}, 204)
