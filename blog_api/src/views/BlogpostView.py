42e8ad7d3ece937fd0e5fc6dab9cea1d2b879d3c
mykolatyniv <ml546@njit.edu>
2019-12-06 14:45:40 -0500

    Task: updated blogpostfile file with new information

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -1,4 +1,4 @@
-#/src/views/BlogpostView.py
+# /src/views/BlogpostView.py
 # app initiliazation
 #####################
 # existing code remain #
@@ -8,33 +8,40 @@
 @blogpost_api.route('/', methods=['POST'])
 @Auth.auth_required
 def create():
-  """
+    """
   Create Blogpost Function
   """
-  # app initiliazation
-  #####################
-  # existing code remain #
-  ######################
-  return custom_response(data, 201)
-
-  # app initiliazation
-  #####################
-  # existing code remain #
-  ######################
-
-@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
-def get_one(blogpost_id):
-  """
-  Get A Blogpost
-  """
-  post = BlogpostModel.get_one_blogpost(blogpost_id)
-  if not post:
-    return custom_response({'error': 'post not found'}, 404)
-  data = blogpost_schema.dump(post).data
-  return custom_response(data, 200)
+    # app initiliazation
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response(data, 201)
 
 
 # app initiliazation
 #####################
 # existing code remain #
 ######################
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+    """
+  Update A Blogpost
+  """
+    req_data = request.get_json()
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    data, error = blogpost_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+    post.update(data)
+
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
