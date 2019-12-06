070c33089891ccbf9244062327f8e3f3bc296352
mykolatyniv <ml546@njit.edu>
2019-12-06 14:29:47 -0500

    Task: updated UserModel file

--- blog_api/src/models/UserModel.py
+++ blog_api/src/models/UserModel.py
@@ -1,56 +1,57 @@
 # src/models/UserModel.py
-from marshmallow import fields, Schema
-import datetime
-from . import db
+#####################
+# existing code remain #
+######################
+from ..app import bcrypt  # add this line
 
 
 class UserModel(db.Model):
     """
-  User Model
-  """
+    User Model
+    """
 
     # table name
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=True)
-    created_at = db.Column(db.DateTime)
-    modified_at = db.Column(db.DateTime)
+
+    #####################
+    # existing code remain #
+    ######################
 
     # class constructor
     def __init__(self, data):
         """
-    Class constructor
-    """
+        Class constructor
+        """
         self.name = data.get('name')
         self.email = data.get('email')
-        self.password = data.get('password')
-        self.created_at = datetime.datetime.utcnow()
-        self.modified_at = datetime.datetime.utcnow()
+        self.password = self.__generate_hash(data.get('password'))  # add this line
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+    #####################
+    # existing code remain #
+    ######################
 
     def update(self, data):
         for key, item in data.items():
+            if key == 'password':  # add this new line
+                self.password = self.__generate_hash(value)  # add this new line
             setattr(self, key, item)
         self.modified_at = datetime.datetime.utcnow()
         db.session.commit()
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+    #####################
+    # existing code remain #
+    ######################
 
-    @staticmethod
-    def get_all_users():
-        return UserModel.query.all()
+    # add this new method
+    def __generate_hash(self, password):
+        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
 
-    @staticmethod
-    def get_one_user(id):
-        return UserModel.query.get(id)
+    # add this new method
+    def check_hash(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
-    def __repr(self):
-        return '<id {}>'.format(self.id)
+    #####################
+    # existing code remain #
+    ######################
