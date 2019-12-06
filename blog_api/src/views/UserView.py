d22ab09b6e1c743e59b753e442eb97834e0435c8
mykolatyniv <ml546@njit.edu>
2019-12-06 14:36:46 -0500

    Task: added new info

--- blog_api/src/views/UserView.py
+++ blog_api/src/views/UserView.py
@@ -1,6 +1,6 @@
 # /src/views/UserView
 
-from flask import request, json, Response, Blueprint
+from flask import request, json, Response, Blueprint, g
 from ..models.UserModel import UserModel, UserSchema
 from ..shared.Authentication import Auth
 
@@ -8,40 +8,62 @@ user_api = Blueprint('user_api', __name__)
 user_schema = UserSchema()
 
 
-@user_api.route('/', methods=['POST'])
-def create():
-    """
-    Create User Function
-    """
-    #####################
-    # existing code remain #
-    ######################
-    return custom_response({'jwt_token': token}, 201)
+#####################
+# existing code remains #
+########################
 
-
-# add this new method
-@user_api.route('/', methods=['GET'])
+@user_api.route('/<int:user_id>', methods=['GET'])
 @Auth.auth_required
-def get_all():
-    users = UserModel.get_all_users()
-    ser_users = user_schema.dump(users, many=True).data
-    return custom_response(ser_users, 200)
-
-
-@user_api.route('/login', methods=['POST'])
-def login():
-    #####################
-    # existing code remain #
-    ######################
-    return custom_response({'jwt_token': token}, 200)
-
-
-def custom_response(res, status_code):
+def get_a_user(user_id):
     """
-    Custom Response Function
+    Get a single user
     """
-    return Response(
-        mimetype="application/json",
-        response=json.dumps(res),
-        status=status_code
-    )
+    user = UserModel.get_one_user(user_id)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['PUT'])
+@Auth.auth_required
+def update():
+    """
+    Update me
+    """
+    req_data = request.get_json()
+    data, error = user_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.update(data)
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['DELETE'])
+@Auth.auth_required
+def delete():
+    """
+    Delete a user
+    """
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.delete()
+    return custom_response({'message': 'deleted'}, 204)
+
+
+@user_api.route('/me', methods=['GET'])
+@Auth.auth_required
+def get_me():
+    """
+    Get me
+    """
+    user = UserModel.get_one_user(g.user.get('id'))
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+#####################
+# existing code remains #
+########################
