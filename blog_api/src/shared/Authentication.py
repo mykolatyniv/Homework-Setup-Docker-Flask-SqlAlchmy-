5db1c75023d22d1bfa816eb2aaa56702f4acaf7e
mykolatyniv <ml546@njit.edu>
2019-12-06 14:34:21 -0500

    Task: created Authentication file

--- blog_api/src/shared/Authentication.py
+++ blog_api/src/shared/Authentication.py
@@ -2,8 +2,7 @@
 import jwt
 import os
 import datetime
-from flask import json, Response, request, g
-from functools import wraps
+from flask import json
 from ..models.UserModel import UserModel
 
 
@@ -50,38 +49,3 @@ class Auth():
     except jwt.InvalidTokenError:
       re['error'] = {'message': 'Invalid token, please try again with a new token'}
       return re
-
-  # decorator
-  @staticmethod
-  def auth_required(func):
-    """
-    Auth decorator
-    """
-    @wraps(func)
-    def decorated_auth(*args, **kwargs):
-      if 'api-token' not in request.headers:
-        return Response(
-          mimetype="application/json",
-          response=json.dumps({'error': 'Authentication token is not available, please login to get one'}),
-          status=400
-        )
-      token = request.headers.get('api-token')
-      data = Auth.decode_token(token)
-      if data['error']:
-        return Response(
-          mimetype="application/json",
-          response=json.dumps(data['error']),
-          status=400
-        )
-        
-      user_id = data['data']['user_id']
-      check_user = UserModel.get_one_user(user_id)
-      if not check_user:
-        return Response(
-          mimetype="application/json",
-          response=json.dumps({'error': 'user does not exist, invalid token'}),
-          status=400
-        )
-      g.user = {'id': user_id}
-      return func(*args, **kwargs)
-    return decorated_auth
