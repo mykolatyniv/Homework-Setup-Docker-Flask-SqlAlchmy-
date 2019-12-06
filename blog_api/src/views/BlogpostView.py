e1dcd10483887d9e9d6aeeae868c99319830c6c4
mykolatyniv <ml546@njit.edu>
2019-12-06 14:45:07 -0500

    Task: updated blogpostfile file with new information

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -1,10 +1,8 @@
 #/src/views/BlogpostView.py
-from flask import request, g, Blueprint, json, Response
-from ..shared.Authentication import Auth
-from ..models.BlogpostModel import BlogpostModel, BlogpostSchema
-
-blogpost_api = Blueprint('blogpost_api', __name__)
-blogpost_schema = BlogpostSchema()
+# app initiliazation
+#####################
+# existing code remain #
+######################
 
 
 @blogpost_api.route('/', methods=['POST'])
@@ -13,23 +11,24 @@ def create():
   """
   Create Blogpost Function
   """
-  req_data = request.get_json()
-  req_data['owner_id'] = g.user.get('id')
-  data, error = blogpost_schema.load(req_data)
-  if error:
-    return custom_response(error, 400)
-  post = BlogpostModel(data)
-  post.save()
-  data = blogpost_schema.dump(post).data
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
   return custom_response(data, 201)
 
+# add this function
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+  """
+  Get All Blogposts
+  """
+  posts = BlogpostModel.get_all_blogposts()
+  data = blogpost_schema.dump(posts, many=True).data
+  return custom_response(data, 200)
 
-def custom_response(res, status_code):
-  """
-  Custom Response Function
-  """
-  return Response(
-    mimetype="application/json",
-    response=json.dumps(res),
-    status=status_code
-  )
+
+# app initiliazation
+#####################
+# existing code remain #
+######################
