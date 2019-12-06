d4a1226c90f475b545549b4b0f995bae73d3d825
mykolatyniv <ml546@njit.edu>
2019-12-06 14:36:11 -0500

    Task: added new info

--- blog_api/src/views/UserView.py
+++ blog_api/src/views/UserView.py
@@ -16,36 +16,32 @@ def create():
     #####################
     # existing code remain #
     ######################
-
     return custom_response({'jwt_token': token}, 201)
+
+
+# add this new method
+@user_api.route('/', methods=['GET'])
+@Auth.auth_required
+def get_all():
+    users = UserModel.get_all_users()
+    ser_users = user_schema.dump(users, many=True).data
+    return custom_response(ser_users, 200)
 
 
 @user_api.route('/login', methods=['POST'])
 def login():
-    req_data = request.get_json()
-
-    data, error = user_schema.load(req_data, partial=True)
-
-    if error:
-        return custom_response(error, 400)
-
-    if not data.get('email') or not data.get('password'):
-        return custom_response({'error': 'you need email and password to sign in'}, 400)
-
-    user = UserModel.get_user_by_email(data.get('email'))
-
-    if not user:
-        return custom_response({'error': 'invalid credentials'}, 400)
-
-    if not user.check_hash(data.get('password')):
-        return custom_response({'error': 'invalid credentials'}, 400)
-
-    ser_data = user_schema.dump(user).data
-
-    token = Auth.generate_token(ser_data.get('id'))
-
-    return custom_response({'jwt_token': token}, 200)
-
     #####################
     # existing code remain #
     ######################
+    return custom_response({'jwt_token': token}, 200)
+
+
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
