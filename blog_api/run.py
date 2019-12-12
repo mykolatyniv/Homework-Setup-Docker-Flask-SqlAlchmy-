7e122d95c6a4f04e926317cc7c145f1cb69ec631
mykolatyniv <ml546@njit.edu>
2019-12-11 21:17:25 -0500

    Fix: fixed files, due to some errors

--- blog_api/run.py
+++ blog_api/run.py
@@ -3,8 +3,19 @@ import os
 
 from src.app import create_app
 
+def set_environment_variables():
+  os.environ["FLASK_ENV"] = "development"
+  os.environ["DATABASE_URL"] = 'postgres://name:password@houst:port/blog_api_db'
+  os.environ["JWT_SECRET_KEY"] = 'hhgaghhgsdhdhdd'
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
-  # run app
-  app.run()
+    set_environment_variables()
+
+    env_name = os.getenv('FLASK_ENV')
+
+    if env_name is None:
+        env_name = 'Development'
+
+    app = create_app(env_name)
+    # run app
+    app.run()
