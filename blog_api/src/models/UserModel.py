35dfc00eedd9bb9a1a0b505669e9b3dd1c114e92
mykolatyniv <ml546@njit.edu>
2019-12-11 21:25:16 -0500

    Fix: fixed files, due to some errors

--- blog_api/src/models/UserModel.py
+++ blog_api/src/models/UserModel.py
@@ -18,6 +18,7 @@ class UserModel(db.Model):
     password = db.Column(db.String(128), nullable=True)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
+    blogposts = db.relationship('BlogpostModel', backref='users', lazy=True)  # add this new line
 
     # class constructor
     def __init__(self, data):
@@ -65,3 +66,16 @@ class UserModel(db.Model):
 
     def __repr(self):
         return '<id {}>'.format(self.id)
+
+    # add this class
+    class UserSchema(Schema):
+        """
+        User Schema
+        """
+        id = fields.Int(dump_only=True)
+        name = fields.Str(required=True)
+        email = fields.Email(required=True)
+        password = fields.Str(required=True)
+        created_at = fields.DateTime(dump_only=True)
+        modified_at = fields.DateTime(dump_only=True)
+        blogposts = fields.Nested(BlogpostSchema, many=True)
