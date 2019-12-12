c8050d22c688e0a91d606000012192cc4c9d5ade
mykolatyniv <ml546@njit.edu>
2019-12-11 20:50:19 -0500

    Fix: fixed docker file

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -1,4 +1,4 @@
-# src/app.py
+#src/app.py
 
 from flask import Flask
 
@@ -6,26 +6,31 @@ from .config import app_config
 from .models import db, bcrypt
 
 # import user_api blueprint
-from .views.UserView import user_api as user_blueprint  # add this line
+from .views.UserView import user_api as user_blueprint
 
 
 def create_app(env_name):
+  """
+  Create app
+  """
+  
+  # app initiliazation
+  app = Flask(__name__)
+
+  app.config.from_object(app_config[env_name])
+
+  # initializing bcrypt and db
+  bcrypt.init_app(app)
+  db.init_app(app)
+
+  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+
+  @app.route('/', methods=['GET'])
+  def index():
     """
-    Create app
+    example endpoint
     """
+    return 'Congratulations! Your part 2 endpoint is working'
 
-    # app initiliazation
-    #####################
-    # existing code remain #
-    ######################
+  return app
 
-    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')  # add this line
-
-    @app.route('/', methods=['GET'])
-    def index():
-        """
-        example endpoint
-        """
-        return 'Congratulations! Your first endpoint is working'
-
-    return app
--- blog_api/src/models/BlogpostModel.py
+++ blog_api/src/models/BlogpostModel.py
@@ -1,7 +1,6 @@
-# src/models/BlogpostModel.py
-#####################
-# existing code remains #
-########################
+# src/models/Blogpost.py
+from . import db
+import datetime
 from marshmallow import fields, Schema
 
 class BlogpostModel(db.Model):
@@ -11,31 +10,44 @@ class BlogpostModel(db.Model):
 
   __tablename__ = 'blogposts'
 
-  #####################
-  # existing code remains #
-  ########################
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(128), nullable=False)
   contents = db.Column(db.Text, nullable=False)
-  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # add this new line
-  #####################
-  # existing code remains #
-  ########################
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  created_at = db.Column(db.DateTime)
+  modified_at = db.Column(db.DateTime)
 
   def __init__(self, data):
-    #####################
-    # existing code remains #
-    ########################
-    self.owner_id = data.get('owner_id) # add this new line
+    self.title = data.get('title')
+    self.contents = data.get('contents')
     self.created_at = datetime.datetime.utcnow()
     self.modified_at = datetime.datetime.utcnow()
 
-  #####################
-  # existing code remains #
-  ########################
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self, data):
+    for key, item in data.items():
+      setattr(self, key, item)
+    self.modified_at = datetime.datetime.utcnow()
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    de.session.commit()
+  
+  @staticmethod
+  def get_all_blogposts():
+    return BlogpostModel.query.all()
+  
+  @staticmethod
+  def get_one_blogpost(id):
+    return BlogpostModel.query.get(id)
 
   def __repr__(self):
     return '<id {}>'.format(self.id)
 
-# add this new class
 class BlogpostSchema(Schema):
   """
   Blogpost Schema
@@ -43,6 +55,5 @@ class BlogpostSchema(Schema):
   id = fields.Int(dump_only=True)
   title = fields.Str(required=True)
   contents = fields.Str(required=True)
-  owner_id = fields.Int(required=True)
   created_at = fields.DateTime(dump_only=True)
   modified_at = fields.DateTime(dump_only=True)
